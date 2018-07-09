7f7774b0210c67ae50665b1ac5bb9291d22bfa72
imran <imran@taskspotting.com>
2018-07-09 22:43:11 +0530

    all

--- wp-content/themes/pashmina/single-top_items.php
+++ wp-content/themes/pashmina/single-top_items.php
@@ -108,7 +108,7 @@ $tag_name = $tags[0]->name;
             <a class="fa fa-envelope-o" href=""></a>
          </div>
          <div class="social-ttl-list">
-          <h5>RECENT TOP PRODUCTS</h5>
+          <h5>RECENT TOP COLLECTION</h5>
           <?php
             $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $args = array( 'post_type' => 'top_items', 'posts_per_page' =>10, 'paged' => $paged );
@@ -130,74 +130,6 @@ $custom_query = new WP_Query($args);
      </div>
    </div>
 
-   <!-- product dtl and card images -->
-<div class="container">
-  <div class="row">
-    
- <div class="col-lg-4 col-xs-12 col-md-4 col-sm-12 pull-right media">
-   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-bordered">
-      <h5 class="panel-body">10 Brown Shade Lipsticks that You Should Try</h5>
-
-       <div class="col-lg-6 col-xs-6">
-         <a class="prouct-silder-img" style="background-image: url(http://gloat.me/wp-content/uploads/2018/07/carousel-women.jpg)" href="/top-10/10-brown-lipsticks-that-you-should-try">
-           <p class="">Brown shades will never look out of style</p>
-         </a>
-      </div>
-
-      <div class="col-lg-6 col-xs-6">
-         <a class="prouct-silder-img" style="background-image: url(http://d1acy2vp0zxghs.cloudfront.net/featured_categories/images/000/000/110/original/26-min.jpg?1487305456)" href="/top-10/10-brown-lipsticks-that-you-should-try">
-         <p class="">Brown shades will never look out of style</p>
-         </a>
-      </div>
-
-       <div class="col-lg-6 col-xs-6">
-         <a class="prouct-silder-img" style="background-image: url(http://gloat.me/wp-content/uploads/2018/07/carousel-women.jpg)" href="/top-10/10-brown-lipsticks-that-you-should-try">
-         <p class="">Brown shades will never look out of style</p>
-         </a>
-      </div>
-
-        <div class="col-lg-6 col-xs-6">
-         <a class="prouct-silder-img" style="background-image: url(https://images-eu.ssl-images-amazon.com/images/I/314nru0lM7L.jpg)" href="/top-10/10-brown-lipsticks-that-you-should-try">
-         <p class="">Brown shades will never look out of style</p>
-         </a>
-       </div>
-      </div>
-
-      <div class="row">
-        <div  class="col-lg-12 col-xs-12 col-md-12 col-sm-12">
-        <h4>You might like these:</h4>
-        <hr>
-        <a href="">
-        <div class="col-lg-3 col-lg-3 col-sm-3 col-xs-3 col-md-3 card-brder-hovr text-center">
-           <img class="img-responsive" src="http://d1acy2vp0zxghs.cloudfront.net/products/images/000/025/456/thumb/Nivea_Fruity_Shine_Lip_Balm_%28Strawberry%29.jpg?1481189411">
-          </div>
-          <div class="col-lg-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 prodt-like-cnt">
-           <span>Nivea Essential Care Lip Balm</span>
-            <h6 class="text-danger">Nivea</h6>
-             <span class=""><span class="">RM8.50</span> <span class="discount-tag">-26%</span> <strike>RM11.60</strike></span>
-             </div>
-            </a>
-         </div>
-       </div>
-
-      <div class="row">
-       <div  class="col-lg-12 col-xs-12 col-md-12 col-sm-12">
-          <hr>
-              <a href="">
-         <div class="col-lg-3 col-sm-3 col-xs-3 col-md-3 card-brder-hovr text-center">
-           <img class="img-responsive" src="http://d1acy2vp0zxghs.cloudfront.net/products/images/000/025/456/thumb/Nivea_Fruity_Shine_Lip_Balm_%28Strawberry%29.jpg?1481189411">
-         </div>
-         <div class="col-lg-9 col-lg-9 col-sm-9 col-xs-9 col-md-9 prodt-like-cnt">
-           <span>Nivea Essential Care Lip Balm</span>
-           <h6 class="text-danger">Nivea</h6>
-           <span class="product-price"><span class="discounted-price">RM8.50</span> <span class="discount-tag">-26%</span> <strike>RM11.60</strike></span>
-        </div>
-         </a>
-     </div>
-     </div>
-    </div>
-
-  </div>
-</div>
+  
 <?php
 get_footer();
