ef543ae3b8bb0da0a45af2845bd0e074e58db588
imran <imran@taskspotting.com>
2018-07-20 18:01:45 +0530

    contest

--- wp-content/themes/pashmina/single-contest.php
+++ wp-content/themes/pashmina/single-contest.php
@@ -10,7 +10,8 @@
 get_header('nomenu'); 
 
 $post_ID = get_the_ID();
-$post = get_post();        
+$post = get_post();  
+
 ?>
 
 
@@ -18,23 +19,8 @@ $post = get_post();
 
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-12 col-md-12">
-				<div id="primary" class="content-area">
-					<main id="main" class="site-main" role="main">
-						<div class="entry-content">
-
-						<a href="<?php esc_url( the_permalink() ); ?>">
-              <h1 class="entry-title"><?php the_title(); ?></h1>
-            </a>
-						<?PHP the_content()?>
-
-						</div><!-- .entry-content -->
-						
-						<a href="getStart">Get Start</a>
-					</main><!-- #main -->
-				</div><!-- #primary -->
-			</div><!-- .col-lg-8 -->
-
+			
+						<?=$post->post_content?>			
 			
 		</div><!-- .row -->
 	</div><!-- .container -->
