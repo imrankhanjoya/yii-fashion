6248625b9b7de6af375cfd32aa1b58549814b834
islam <isla@gmail.com>
2018-08-06 13:26:22 +0530

    discuss

--- wp-content/themes/pashmina/discuss.php
+++ wp-content/themes/pashmina/discuss.php
@@ -43,14 +43,131 @@ get_header();
 
     <div class="container discussion-page">
         <div class="row">
-            
-            <?php $custom_query = new WP_Query('post_type="discuss"'); // exclude category 9
+            <div class="col-lg-9 col-md-9">
+              <?php $custom_query = new WP_Query('post_type="discuss"'); // exclude category 9
             while($custom_query->have_posts()) : $custom_query->the_post(); ?>
 
                 <?PHP  get_template_part('template-parts/content-discuss','page'); ?>
 
             <?php endwhile; ?>
             <?php wp_reset_postdata(); // reset the query ?>
+            </div>
+            <div class="col-lg-3">
+      <div class="col-lg-12 pd-0">
+        <h5><b>Top Contributors</b></h5>
+        <p class="p-style">Peple who started the most discussions on Talks.</p>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>Pawet Kadysz<i class=" icon-type fa fa-comment-o"></i></b>103</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>agnieszka bladzik<i class=" icon-type fa fa-comment-o"></i></b>84</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>lan prince<i class=" icon-type fa fa-comment-o"></i></b>79</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>noun <i class=" icon-type fa fa-comment-o"></i></b>76</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>maciej korsan<i class=" icon-type fa fa-comment-o"></i></b>72</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>tigg<i class=" icon-type fa fa-comment-o"></i></b>69</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>Ron dadoo<i class=" icon-type fa fa-comment-o"></i></b>42</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>Artor tobocki<i class=" icon-type fa fa-comment-o"></i></b>32</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class="h6-type"><b>mariusz rzapkowski<i class=" icon-type fa fa-comment-o"></i></b>24</h6>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-2 pd-0">
+           <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+        </div>
+        <div class="col-lg-10 pd-0">
+          <h6 class=""><b>michat kulesza<i class=" icon-type fa fa-comment-o"></i></b>12</h6>
+        </div>
+      </div>
+      <div class="col-lg-12 pd-0">
+        <h5><b>Unanswered Talks</b></h5>
+        <h6 class="p-style">Discussions with no comments. be first to to post comment.</h6>
+      </div>
+      <div class="row">
+          <div class="col-lg-2 pd-0">
+             <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+          </div>
+          <div class="col-lg-10 pd-0">
+            <h6 class=""><b>ponzu </b><span class="p-style"> posted</span></h6>
+          </div>
+          <div class="col-lg-12">
+            <h6 class="p-style">I’ve never use sunscreen & only rely on my foundation SPF which I know is not good for skin. </h6>
+          </div>
+          <div class="col-lg-12">
+            <p class=""><i class="icon-type fa fa-comment-o"></i> 0 Comments</p>
+          </div> 
+        </div>
+        <div class="row">
+          <div class="col-lg-2 pd-0">
+             <img class="img-responsive user-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWbbZHtGYKzepy6xsRyy1ZX9x2wLe_Qbq7s9-49slCtLCmdcFs">
+          </div>
+          <div class="col-lg-10 pd-0">
+            <h6 class=""><b>ponzu </b><span class="p-style"> posted</span></h6>
+          </div>
+          <div class="col-lg-12">
+            <h6 class="p-style">I know is not good for skin. </h6>
+          </div> 
+        </div>
+      </div>
+            
             
         </div>
     </div>
