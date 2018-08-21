be15a536efd3b75cf4de4b90ff3b79da4986047c
imran <imran@taskspotting.com>
2018-08-21 15:02:35 +0530

    on

--- wp-content/themes/pashmina/functions.php
+++ wp-content/themes/pashmina/functions.php
@@ -119,13 +119,14 @@ function pashmina_scripts() {
 
 	// Enqueue Swiper
 	//IKJ wp_enqueue_script( 'swiper', get_template_directory_uri() . '/js/swiper.min.js', array( 'jquery' ), '4.0.6', '' );
+	wp_enqueue_script( 'OneSignalSDK',"https://cdn.onesignal.com/sdks/OneSignalSDK.js", array(),null, '');
 	wp_enqueue_script( 'bootstrapjs',"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", array( 'jquery' ), '4.0.6', '' );
-
 	// Enqueue FontAwesome
 	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.4.0', '' );
 
 	// Custom JS
 	wp_enqueue_script( 'pashmina-custom', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), '', true );
+	wp_enqueue_script( 'OneSignalSDK-loc', get_template_directory_uri() . '/js/OneSignal.js', array( 'OneSignalSDK','bootstrapjs' ), '', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
