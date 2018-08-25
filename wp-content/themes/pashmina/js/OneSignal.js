2d50d2d9210dffb5021a9b6db04fa6bd57635b4d
imran <imran@taskspotting.com>
2018-08-26 02:23:44 +0530

    ll

--- wp-content/themes/pashmina/js/OneSignal.js
+++ wp-content/themes/pashmina/js/OneSignal.js
@@ -36,7 +36,6 @@ var OneSignal = window.OneSignal || [];
                         
                     });
                 });
-                //jQuery("#pushNotificaton").modal('show');
                 jQuery(".push-my-div").hide();
             }else{
                 if(getCookie('closepush')==null){
