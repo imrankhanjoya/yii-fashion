b3dae2d12ec944b5373ea2f51f5ec56e49636e9b
imrankhan <imran@gmail.com>
2018-12-02 14:29:09 +0530

    all

--- gyii/frontend/views/site/index.php
+++ gyii/frontend/views/site/index.php
@@ -50,10 +50,10 @@ $this->title = $m_title;
          <div class="p-2 p-lg-0 cards-box mb-2 mb-lg-0">
             <div class="row">
                <div class="col-lg-12 col-12 col-md-12 mt-0 mt-lg-4">
-                  <div class="p-3" style="background-image: url(http://www.gloat.me/wp-content/themes/pashmina/images/banner_2.svg);">
+                  <div class="p-3" style="background-image: url(img/banner/banner_2.svg);">
                      <div class="text-center col-lg-12">
                         <p>
-                           <img class="product-ttl-heig" src="http://www.gloat.me/wp-content/themes/pashmina/images/woman-with-long-hair.svg">Wash your face with ice water or simply rub and ice cube with a tsp of honey on the face for instant face lift
+                           <img class="product-ttl-heig" src="img/banner/woman-with-long-hair.svg">Wash your face with ice water or simply rub and ice cube with a tsp of honey on the face for instant face lift
                         </p>
                         <button class="text-center btn rounded-0">Stay in Touch for Beauty Tips</button>
                      </div>
@@ -96,16 +96,16 @@ $this->title = $m_title;
          <div class="p-2 p-lg-0 cards-box mb-2 mb-lg-0">
             <div class="row">
                <div class="col-lg-4">
-                  <a class="top-banners top_banner-t mt-0 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(http://gloat.me/wp-content/uploads/2018/07/LorealParisDK0213136586v2Logo.jpg);" href="#"></a>
+                  <a class="top-banners top_banner-t mt-0 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(img/banner/LorealParisDK0213136586v2Logo.jpg);" href="#"></a>
                </div>
                <div class="col-lg-8">
-                  <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(http://www.gloat.me/wp-content/themes/pashmina/images/discuss.png);" href="#"></a>
+                  <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(img/banner/discuss.png);" href="#"></a>
                   <div class="row">
                      <div class="col-lg-6 col-12">
-                        <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(http://www.gloat.me/wp-content/themes/pashmina/images/Product.png);" href="#"></a>
+                        <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(img/banner/Product.png);" href="#"></a>
                      </div>
                      <div class="col-lg-6 col-12">
-                        <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(http://gloat.me/wp-content/uploads/2018/07/lakme.png);" href="#"></a>
+                        <a class="top-banners mt-2 mt-lg-4 p-3 text-center position-relative align-items-center d-flex" style="background-image: url(img/banner/lakme.png);" href="#"></a>
                      </div>
                   </div>
                </div>
